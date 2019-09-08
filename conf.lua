0f5049dd458d1d8d795040b235bbaaf130715cf1
Rom DuPlain <rom.duplain@gmail.com>
2019-09-07 23:29:24 -0400

    Add base files for Lua/LÖVE (love2d.org).

---
+++ conf.lua
@@ -0,0 +1,6 @@
+-- Configuration for LÖVE.
+-- https://love2d.org/wiki/Config_Files
+
+function love.conf(t)
+  t.window.fullscreentype = "desktop"
+end
---
+++ main.lua
@@ -0,0 +1,14 @@
+-- Initialize.
+function love.load()
+
+end
+
+-- Update, on loop.
+function love.update()
+
+end
+
+-- Draw, on loop.
+function love.draw()
+  love.graphics.print("Hello, world!")
+end
