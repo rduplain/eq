16d3231e793db8918f448b1ab4e966dee05b10da
Rom DuPlain <rom.duplain@gmail.com>
2020-01-20 23:43:32 -0800

    Add project-local LuaJIT installation.

---
+++ .Makefile.d-init.mk
@@ -0,0 +1,14 @@
+MAKEFILE_D_INIT_MK := $(abspath $(lastword $(MAKEFILE_LIST)))
+
+MAKEFILE_D_URL ?= https://github.com/rduplain/Makefile.d.git
+MAKEFILE_D_REV ?= v1.4.1 # Use --ref instead of --tag below if untagged.
+
+QWERTY_SH_URL ?= https://qwerty.sh
+QWERTY_SH ?= curl --proto '=https' --tlsv1.2 -sSf $(QWERTY_SH_URL) | sh -s -
+
+.Makefile.d/%.mk: .Makefile.d/path.mk
+	@touch $@
+
+.Makefile.d/path.mk: $(MAKEFILE_D_INIT_MK)
+	$(QWERTY_SH) -f -o .Makefile.d --tag $(MAKEFILE_D_REV) $(MAKEFILE_D_URL)
+	@touch $@
--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
-game:
+# Requires `love`, love2d.org. Developed on LÖVE 11.3.
+game: love-command
 	@love .
+
+MAKEFILE := $(lastword $(MAKEFILE_LIST))
+
+include .Makefile.d-init.mk
+include .Makefile.d/command.mk
+
+# Note: LÖVE ships with its own LuaJIT implementation.
+include .Makefile.d/luajit.mk
+
+lua: lua-command
+	@$(LUA)
